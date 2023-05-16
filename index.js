d8c28741cf8d99e546c1422b51bd632c11438c8a
unknown <dbarman8697@gmail.com>
2023-05-17 03:57:25 +0530

    Dwipayan-Barman-Portfolio

--- index.js
+++ index.js
@@ -29,3 +29,29 @@ document
     redirect();
     download();
   });
+
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+});
+
+document.onkeydown = function (e) {
+  if (event.keycode == 123) {
+    return false;
+  }
+
+  if (e.ctrlKey && e.shiftKey && e.keycode == "I".charCodeAt) {
+    return false;
+  }
+
+  if (e.ctrlKey && e.shiftKey && e.keycode == "C".charCodeAt(0)) {
+    return false;
+  }
+
+  if (e.ctrlKey && e.shiftKey && e.keycode == "J".charCodeAt(0)) {
+    return false;
+  }
+
+  if (e.ctrlKey && e.shiftKey && e.keycode == "I".charCodeAt(0)) {
+    return false;
+  }
+};
